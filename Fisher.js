ff137b3cf4e08ca27c8781859a46d3aebd1f3169
Yaroslav <6715414@gmail.com>
2023-04-25 11:35:23 +0300

    added to save the last message id in file

--- Fisher.js
+++ Fisher.js
@@ -1,34 +1,46 @@
 import { DiscordFishAPI } from "./DiscordFishAPI.js";
 import { Logs } from "./Logs.js";
+import { FileHandler } from "./FileHandler.js";
 import { payloads } from "./payloads.js";
 
 export class Fisher {
-  constructor(name, userId, DISCORD_TOKEN, CHANNEL_ID) {
+  constructor(name, userId, DISCORD_TOKEN) {
     this.name = name;
     this._userId = userId;
-    this._CHANNEL_ID = CHANNEL_ID;
     this._DISCORD_TOKEN = DISCORD_TOKEN;
+    this._prevMessageId = null;
+    this._filePath = `./data/lastMessId_${this.name}.txt`;
 
     this._init();
   }
 
   _init() {
     this.discordApi = new DiscordFishAPI(this._userId, this._DISCORD_TOKEN);
-    this._prevMessageId = null;
+
+    if (!this._prevMessageId) {
+      FileHandler.readFile(this._filePath)
+        .then((id) => {
+          if (id) {
+            this._prevMessageId = id;
+          }
+        })
+        .catch((err) => {
+          Logs.error(err);
+        });
+    }
   }
 
   async _isCanFishing() {
     let isFishing = false;
 
-    const messages = await this.discordApi.getUserMessagesByChannelId(
-      this._CHANNEL_ID
-    );
+    const messages = await this.discordApi.getUserMessagesByChannelId();
     const lastMessage = messages.pop();
     if (lastMessage) {
       if (this._prevMessageId === lastMessage.id) return false;
 
       isFishing = lastMessage.content.includes("again!");
       this._prevMessageId = lastMessage.id;
+      FileHandler.writeFile(this._filePath, this._prevMessageId);
     }
 
     return isFishing;
@@ -38,9 +50,12 @@ export class Fisher {
     const isFishing = await this._isCanFishing();
 
     if (isFishing) {
-      Logs.info("You can fish again!");
-      this.discordApi.sendCommand(payloads.fish);
-      Logs.info("Fished");
+      await this.discordApi.sendCommand(payloads.fish);
+
+      const info = await this.discordApi.getInfoByMessageId();
+      if (info) {
+        Logs.info(`Fished - ${info.name} - ${info.price}`);
+      }
     }
   }
 
