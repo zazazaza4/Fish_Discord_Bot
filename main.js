508f584d22900e763b3b5b2adcd18645f9d14c57
Yaroslav <6715414@gmail.com>
2023-04-25 11:32:15 +0300

    changed main.js

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 import * as dotenv from "dotenv";
 import { Fisher } from "./Fisher.js";
 
-<<<<<<< HEAD
 dotenv.config();
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const USER_ID = process.env.USER_ID;
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-=======
-const CHANNEL_ID = "957364171215896646";
-const USER_ID = "";
-const DISCORD_TOKEN = "";
->>>>>>> cae14df4802b46732e51dead5171067953ea2597
 
 const fisher = new Fisher("Nikola", USER_ID, DISCORD_TOKEN, CHANNEL_ID);
 let humanMargin = 0;
